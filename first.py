88d81e7bc292749162535a686ab39c8433d0d0f2
davidseungjin <ryonlee@gmail.com>
2019-06-23 21:20:14 -0700

    py

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
 print('Hello World')
+
+
+print("This is testing double quote to use single quote in the text sentence like this' this' this'")
+
+
+print ('This is one line', "this is another sentence using double quote")
+print ('This is one line', end=' ')
+print("this is another sentence using double quote")
+

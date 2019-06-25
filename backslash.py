234b6f47de4844a91e49fdb5ce95f7d177247dce
davidseungjin <ryonlee@gmail.com>
2019-06-25 07:57:38 -0700

    py

---
+++ backslash.py
@@ -0,0 +1,5 @@
+print("two slash with double quotation marks //")
+print('two slash with single quotation marks //')
+
+print("two back slash with double quotation marks \\")
+print('two backslash with single quotation marks')
